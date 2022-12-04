d52c904d8191f1f80055e435b8fbf025041dac28
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2022-12-04 13:49:37 +0530

    Update settings.py

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/heartcare/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/')
+    os.path.join(BASE_DIR, 'heartcare/static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
