d9a97b65dbcd72a6979ce064cd861ab5b6b4e010
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2022-12-04 13:44:33 +0530

    Update settings.py

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'heartcare/static')
+    os.path.join(BASE_DIR, '/static/')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
