7f4fc90153e6db247d5ed25846dcd3fdc82f8730
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2023-02-03 21:00:53 +0530

    Update urls.py

--- heartcare/urls.py
+++ heartcare/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('appointment/', include('appointment.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL,
+#                           document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
