f853d8e831e735b64433ed8a8de3602252aa5b26
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2023-02-03 21:59:38 +0530

    Update settings.py

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import datetime
 import os
-import psycopg2
+import psycopg2-binary
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
