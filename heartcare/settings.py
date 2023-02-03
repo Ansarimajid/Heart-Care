5654351b94685ce4b2f53ba4d4a7ec770a697ad2
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2023-02-03 21:03:25 +0530

    Update settings.py

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'heartcare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
