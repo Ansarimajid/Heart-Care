ac94eca3967daf2003db3ec92e0dda14d647aa70
Ansari Majid Ali <86536394+Ansarimajid@users.noreply.github.com>
2023-02-09 00:39:24 +0530

    Update settings.py

--- heartcare/settings.py
+++ heartcare/settings.py
@@ -83,9 +83,8 @@ WSGI_APPLICATION = 'heartcare.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
 
 
 
